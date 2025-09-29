3156df81b8529b6ab016f1fbd4d777252cbd9a0c
Brandon J <bsjulao@gmail.com>
2025-09-28 18:36:21 -0700

    Commit - Printouts with getters

--- lab14color.cpp
+++ lab14color.cpp
@@ -16,18 +16,29 @@ private:
     int blue;
 
 public:
+    //Setters
     void setRed(int r) {red = r; }
     void setGreen(int g) {green =g; }
     void setBlue(int b) {blue =b; }
 
+    //Getters
     int getRed() const {return red; }
     int getGreen() const {return green; }
     int getBlue() const {return blue; }
 };
 
 int main() {
+    Color c;
+    c.setRed(100);
+    c.setGreen(150);
+    c.setBlue(200);
+
     cout << "Color Class starting...!\n";
 
-    
+    //Printouts with getters
+    cout << "Red: " << c.getRed()
+         << " Green: " << c.getGreen()
+         << " Blue: " << c.getBlue() << "\n";
+
     return 0;
 }
