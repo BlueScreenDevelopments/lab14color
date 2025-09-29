728afb42897a4e1ba097a5c6108d40ffedd17933
Brandon J <bsjulao@gmail.com>
2025-09-28 18:56:56 -0700

    Commit - printouts, parameters, constructors

--- lab14color.cpp
+++ lab14color.cpp
@@ -16,12 +16,18 @@ private:
     int blue;
 
 public:
-    //Setters
+    //default constr
+    Color() : red(0), green(0), blue (0) {}
+
+    //parameters constr
+    Color(int r, int g, int b) : red(r), green(g), blue(b){}
+
+    //setters
     void setRed(int r) {red = r; }
     void setGreen(int g) {green =g; }
     void setBlue(int b) {blue =b; }
 
-    //Getters
+    //getters
     int getRed() const {return red; }
     int getGreen() const {return green; }
     int getBlue() const {return blue; }
