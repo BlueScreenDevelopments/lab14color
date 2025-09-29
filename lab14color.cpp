3f4e7dbfc763c3f8097f68e1484da011de6d5f2f
Brandon J <bsjulao@gmail.com>
2025-09-28 18:24:12 -0700

    Initial Commit

---
+++ lab14color.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+Brandon Julao
+COMSC-210-5470
+09.28.25
+Color Class
+*/
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+
+public:
+    // Empty for now
+};
+
+int main() {
+    std::cout << "Color Class starting...!\n";
+    return 0;
+}
